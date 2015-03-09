3e44a92c4517e629a9806d56837385c3f8c4d5c1
shanerbn <shane@converted.co.uk>
2015-03-09 00:07:20 +0000

    added devise error message code for styling

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,23 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+<div class="alert alert-danger alert-dismissable">
+<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span>
+<span class="sr-only">Close</span>
+</button>
+
+<h2>#{sentence}</h2>
+#{messages}
+</div>
+HTML
+
+    html.html_safe
+end
+  end
