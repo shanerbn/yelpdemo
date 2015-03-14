4f3ff52c0fb74f60a9b84ea0ff21d9204eae04f0
shanerbn <shane@converted.co.uk>
2015-03-14 00:05:15 +0000

    ordered reviews to newest first

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
-    @reviews = Review.where(restaurant_id: @restaurant.id)
+    @reviews = Review.where(restaurant_id: @restaurant.id).order("created_at DESC")
     if @reviews.blank?
       @avg_rating = 0
       else
