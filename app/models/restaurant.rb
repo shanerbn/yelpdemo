9ad2c11035753215ef66e59529906f7d50b49894
shanerbn <shane@converted.co.uk>
2015-03-17 10:11:10 +0000

    added field validations and placeholder text

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,13 @@ class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   has_many  :reviews
+
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+                                    message: "must be in the format (123) 456 7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+                                message: "must be in the format of http://www."}
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+                                message: "must be in the format of 250 Fifth Avenue, New york, NY 10019"  }
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :restuarant
+
+  validates :rating, :comment, presence: true
+  validates :rating, numericality: {
+                only_integer: true,
+                greater_than_or_equal_to: 1,
+                less_than_or_equal_to: 5,
+                message: "Can only be a whole number between 1 and 5"
+                   }
 end
